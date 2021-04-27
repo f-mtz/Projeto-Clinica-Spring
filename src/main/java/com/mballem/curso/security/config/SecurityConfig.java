8fe3b45d672af41225461eef06e82d6981abc703
SOLUTIS\felipe.ribeiro <felipe.ribeiro@solutis.com.br>
2021-04-26 23:56:54 -0300

    verificando autenticidade do login + criptografia de senhas

--- src/main/java/com/mballem/curso/security/config/SecurityConfig.java
+++ src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -1,11 +1,19 @@
 package com.mballem.curso.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.mballem.curso.security.service.UsuarioService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UsuarioService service;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -25,6 +33,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 				.logout()
 				.logoutSuccessUrl("/");
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.userDetailsService(service).passwordEncoder(new BCryptPasswordEncoder());
 	}
 	
 	
